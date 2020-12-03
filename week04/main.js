f13f3e703201cfd4971becd9a24c1efc66da0886
9managMent <alexklassen2001@web.de>
2020-12-03 11:48:15 +0100

    Woche 5 Übungen bearbeitet und eins nice Animation hinzugefügt

--- week04/main.js
+++ week04/main.js
@@ -13,9 +13,18 @@ function render() {
 	}
 }
 
-function main() {
+const translationVector = [
+	-0.2,0,0
+]; 
 
-	
+viewMatrix = [
+	0.1767766922712326, -0.0589255653321743, -0.013334667310118675, 0, 
+	0, 0.2357022613286972, -0.006667333655059338, 0, 
+	-0.1767766922712326, -0.0589255653321743, -0.013334667310118675, 0, 
+	0, 0, -0.8801880478858948, 1];
+
+
+function main() {
 	// Get canvas and setup WebGL context
     const canvas = document.getElementById("gl-canvas");
 	gl = canvas.getContext('webgl2');
@@ -33,7 +42,23 @@ function main() {
 	objects.push(new Object3D(new River()));
 	objects.push(new Object3D(new Island()));
 
-	render();
+	const matloc = gl.getUniformLocation(program, "translationVector");
+	gl.uniform3fv(matloc, translationVector);
+
+	const viewloc = gl.getUniformLocation(program, "viewMatrix");
+	gl.uniformMatrix4fv(viewloc, false, viewMatrix);
+
+	setInterval(function(){
+		gl.clearColor(1.0,1.0,1.0,1.0);
+		var rotMat = [];
+		mat4.fromYRotation(rotMat, 0.01);
+		mat4.multiply(viewMatrix,viewMatrix,rotMat);
+		gl.uniformMatrix4fv(viewloc, false, viewMatrix);
+		render();
+	},
+	6.1
+	)
+	
 };
 
 main();
